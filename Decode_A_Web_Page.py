03179d541ff802fe051e4efb9ac94b08a50771e5
LonwaboMvovo <44nator@gmail.com>
2020-04-26 23:51:42 +0200

    added decode a web page problem

--- Decode_A_Web_Page.py
+++ Decode_A_Web_Page.py
@@ -1,1 +1,11 @@
 # https://www.practicepython.org/exercise/2014/06/06/17-decode-a-web-page.html
+import requests
+from bs4 import BeautifulSoup
+
+ny_times_url = 'https://www.nytimes.com/'
+r = requests.get(ny_times_url)
+soup = BeautifulSoup(r.text, features='html.parser')
+
+for story_heading in soup.find_all(class_ = 'css-1ez5fsm'):
+    try: print(f'\n{story_heading.h2.text.strip()}')
+    except: print("Couldn't find what your looking for")
