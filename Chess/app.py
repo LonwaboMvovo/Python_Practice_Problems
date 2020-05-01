4ac04e9f2d6f54fbf691b7bcff88dd905fbc7649
LonwaboMvovo <44nator@gmail.com>
2020-05-01 17:28:55 +0200

    added option to offer draw

--- Chess/app.py
+++ Chess/app.py
@@ -14,12 +14,22 @@ while True:
         player_move = input(f'\n{player}: ').lower()
         if player_move == 'help':
             print('''\nMove notation:
-"Ke2-e4" moves king on e2 to e4
+"Ex: Ke2-e4" moves king on e2 to e4\n
 (P) for pawn, (R) for Rook, (N) for Knight, (B) for bishop, (Q) for Queen and (K) for king
 "O-O" for king-side castling
 "O-O-O" for queen-side castling
 "=" to offer draw
 "Resign" to resign from game''')
+        elif player_move == '=':
+            while True:
+                try:
+                    draw_response = input(f'\n{player} offers you a draw. Do you accept? (Y)es or (N)o\n{"Black: " if player == "White" else "White: "}').lower()
+                    assert draw_response == 'y' or draw_response == 'n'
+                    break
+                except: print('\nInvalid response. (Y) for yes you would like to accept the draw offer of (N) for no you would like to continue playing.')
+            if draw_response == 'y':
+                print("\nGame ends in a draw!")
+                break
         else:
             assert ((player_move[0] == 'r' or player_move[0] == 'n' or player_move[0] == 'b' or player_move[0] == 'q' or player_move[0] == 'k' or player_move[0] == 'p')
             and (player_move[1] == 'a' or player_move[1] == 'b' or player_move[1] == 'c' or player_move[1] == 'd' or player_move[1] == 'e' or player_move[1] == 'f' or player_move[1] == 'g' or player_move[1] == 'h')
